6f204df9c8f4b4f019a0b373b346f53ddd586814
sanip005 <40523104+sanip005@users.noreply.github.com>
2018-08-08 12:44:07 -0500

    Added new method

--- DemoJenkins/src/Calc.java
+++ DemoJenkins/src/Calc.java
@@ -4,4 +4,12 @@ public class Calc {
 	public int add(int x, int y) {
 		return x + y;
 	}
+	
+	public int subs(int x, int y){
+		if(x > y){
+			return x - y;
+		} else {
+			return y - x;
+		}
+	}
 }
