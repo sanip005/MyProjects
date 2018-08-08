ee5de2e0e1f50afc0cd7653983065cb1d3f3267a
Sanip Munankarmi <sanip@Sanips-MacBook-Pro.local>
2018-08-08 12:19:47 -0500

    Add DemoJenkins/

---
+++ DemoJenkins/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Calc cal = new Calc();
+		
+		int a = cal.add(4,  5);
+		
+		System.out.println("The result is "+ a);
+		
+	
+	}
+
+}
